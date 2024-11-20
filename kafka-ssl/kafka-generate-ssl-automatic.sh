707c006d40f6f70bbb6c907751bdb6ed50a401e3
azkafr92 <azkafr92@gmail.com>
2024-11-20 15:31:53 +0700

    define hostname/CN

--- kafka-ssl/kafka-generate-ssl-automatic.sh
+++ kafka-ssl/kafka-generate-ssl-automatic.sh
@@ -15,7 +15,7 @@ KEYSTORE_SIGNED_CERT="cert-signed"
 COUNTRY=$COUNTRY
 STATE=$STATE
 OU=$ORGANIZATION_UNIT
-CN=localhost
+CN=$HOSTNAME
 LOCATION=$CITY
 PASS=$PASSWORD
 
@@ -50,48 +50,48 @@ echo "Welcome to the Kafka SSL keystore and trust store generator script."
 trust_store_file=""
 trust_store_private_key_file=""
 
-  if [ -e "$TRUSTSTORE_WORKING_DIRECTORY" ]; then
-    file_exists_and_exit $TRUSTSTORE_WORKING_DIRECTORY
-  fi
+if [ -e "$TRUSTSTORE_WORKING_DIRECTORY" ]; then
+  file_exists_and_exit $TRUSTSTORE_WORKING_DIRECTORY
+fi
 
-  mkdir $TRUSTSTORE_WORKING_DIRECTORY
-  echo
-  echo "OK, we'll generate a trust store and associated private key."
-  echo
-  echo "First, the private key."
-  echo
+mkdir $TRUSTSTORE_WORKING_DIRECTORY
+echo
+echo "OK, we'll generate a trust store and associated private key."
+echo
+echo "First, the private key."
+echo
 
-  openssl req -new -x509 -keyout $TRUSTSTORE_WORKING_DIRECTORY/ca-key \
-    -out $TRUSTSTORE_WORKING_DIRECTORY/ca-cert -days $VALIDITY_IN_DAYS -nodes \
-    -subj "/C=$COUNTRY/ST=$STATE/L=$LOCATION/O=$OU/CN=$CN"
+openssl req -new -x509 -keyout $TRUSTSTORE_WORKING_DIRECTORY/ca-key \
+  -out $TRUSTSTORE_WORKING_DIRECTORY/ca-cert -days $VALIDITY_IN_DAYS -nodes \
+  -subj "/C=$COUNTRY/ST=$STATE/L=$LOCATION/O=$OU/CN=$CN"
 
-  trust_store_private_key_file="$TRUSTSTORE_WORKING_DIRECTORY/ca-key"
+trust_store_private_key_file="$TRUSTSTORE_WORKING_DIRECTORY/ca-key"
 
-  echo
-  echo "Two files were created:"
-  echo " - $TRUSTSTORE_WORKING_DIRECTORY/ca-key -- the private key used later to"
-  echo "   sign certificates"
-  echo " - $TRUSTSTORE_WORKING_DIRECTORY/ca-cert -- the certificate that will be"
-  echo "   stored in the trust store in a moment and serve as the certificate"
-  echo "   authority (CA). Once this certificate has been stored in the trust"
-  echo "   store, it will be deleted. It can be retrieved from the trust store via:"
-  echo "   $ keytool -keystore <trust-store-file> -export -alias CARoot -rfc"
+echo
+echo "Two files were created:"
+echo " - $TRUSTSTORE_WORKING_DIRECTORY/ca-key -- the private key used later to"
+echo "   sign certificates"
+echo " - $TRUSTSTORE_WORKING_DIRECTORY/ca-cert -- the certificate that will be"
+echo "   stored in the trust store in a moment and serve as the certificate"
+echo "   authority (CA). Once this certificate has been stored in the trust"
+echo "   store, it will be deleted. It can be retrieved from the trust store via:"
+echo "   $ keytool -keystore <trust-store-file> -export -alias CARoot -rfc"
 
-  echo
-  echo "Now the trust store will be generated from the certificate."
-  echo
+echo
+echo "Now the trust store will be generated from the certificate."
+echo
 
-  keytool -keystore $TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILENAME \
-    -alias CARoot -import -file $TRUSTSTORE_WORKING_DIRECTORY/ca-cert \
-    -noprompt -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$OU, CN=$CN" -keypass $PASS -storepass $PASS
+keytool -keystore $TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILENAME \
+  -alias CARoot -import -file $TRUSTSTORE_WORKING_DIRECTORY/ca-cert \
+  -noprompt -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$OU, CN=$CN" -keypass $PASS -storepass $PASS
 
-  trust_store_file="$TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILENAME"
+trust_store_file="$TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILENAME"
 
-  echo
-  echo "$TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILENAME was created."
+echo
+echo "$TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILENAME was created."
 
-  # don't need the cert because it's in the trust store.
-  rm $TRUSTSTORE_WORKING_DIRECTORY/$CA_CERT_FILE
+# don't need the cert because it's in the trust store.
+rm $TRUSTSTORE_WORKING_DIRECTORY/$CA_CERT_FILE
 
 echo
 echo "Continuing with:"
@@ -114,7 +114,7 @@ echo "           the FQDN. Some operating systems call the CN prompt 'first / la
 
 keytool -keystore $KEYSTORE_WORKING_DIRECTORY/$KEYSTORE_FILENAME \
   -alias localhost -validity $VALIDITY_IN_DAYS -genkey -keyalg RSA \
-   -noprompt -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$OU, CN=$CN" -keypass $PASS -storepass $PASS
+  -noprompt -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$OU, CN=$CN" -keypass $PASS -storepass $PASS
 
 echo
 echo "'$KEYSTORE_WORKING_DIRECTORY/$KEYSTORE_FILENAME' now contains a key pair and a"
@@ -164,6 +164,6 @@ echo "   (that was fulfilled)"
 echo " - '$KEYSTORE_SIGNED_CERT': the keystore's certificate, signed by the CA, and stored back"
 echo "    into the keystore"
 
-  rm $KEYSTORE_SIGN_REQUEST_SRL
-  rm $KEYSTORE_SIGN_REQUEST
-  rm $KEYSTORE_SIGNED_CERT
+rm $KEYSTORE_SIGN_REQUEST_SRL
+rm $KEYSTORE_SIGN_REQUEST
+rm $KEYSTORE_SIGNED_CERT
